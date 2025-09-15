41c6b3ffe4a8074224c52f30f194e8607a8bebed
Ana Ciobotaru <ana.medintu@gmail.com>
2025-09-15 11:17:05 +0300

    Add cart class and integrated the functionalities to  main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 def main():
     # Cream instanta ProductManager
@@ -21,6 +23,24 @@ def main():
     # Afisam valoarea totala a inventarului
     print("\n Total value calculation:")
     manager.total_value()
+    
+    # Creare instanata Cart
+    cart = Cart()
+
+    # Selecteaza 3 produse aleatoriu din inventar
+    selected_products = random.sample(manager.products, 3)
+
+    # Adauga produsele selectate in cos
+    for product in selected_products:
+        cart.add_to_cart(product)
+
+    # Afiseaza continutul cosului
+    print("\n--- Cart Contents ---")
+    cart.display_cart()
+
+    # Afiseaza valoarea totala de plata
+    print("\n--- Cart Total ---")
+    cart.calculate_total()    
 
 if __name__ == "__main__":
     main()
